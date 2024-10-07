04f431d2d0192cfedfda5b26c7a287456db8c92f
AnjuOmanakuttan028 <anjuoman1307@gmail.com>
2024-10-06 21:26:28 -0400

    Second Commit

--- app/src/main/java/com/example/cropcrew_capstoneproject/MainActivity.java
+++ app/src/main/java/com/example/cropcrew_capstoneproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cropcrew_capstoneproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.cropcrew_capstoneproject.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ActivityMainBinding mainBinding;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mainBinding.getRoot();
+        setContentView(view);
+        init();
+    }
+    public void init(){
+        mainBinding.btnSignUp.setOnClickListener(this);
+        mainBinding.btnLogin.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==mainBinding.btnLogin.getId()){
+            intent = new Intent(MainActivity.this, Login.class);
+            startActivity(intent);
+        }
+        else if(v.getId()==mainBinding.btnSignUp.getId()){
+            intent = new Intent(MainActivity.this, SignUp.class);
+            startActivity(intent);
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/cropcrew_capstoneproject/SignUp.java
@@ -0,0 +1,31 @@
+package com.example.cropcrew_capstoneproject;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.cropcrew_capstoneproject.databinding.ActivitySignUpBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUp extends AppCompatActivity {
+
+    ActivitySignUpBinding signUpBinding;
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up);
+        firebaseAuth = FirebaseAuth.getInstance();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
